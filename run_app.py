35aff3b9cdd0b53ce63bca34d2b2eaaf4e923e58
Carlos Ochoa <charles0658@gmail.com>
2025-02-23 13:10:38 -0300

    reading from sm service

--- run_app.py
+++ run_app.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from google.cloud import secretmanager
 
 from app.model import Model
 
@@ -7,12 +8,20 @@ import uvicorn
 
 from app.endpoints import app
 
+def get_secret(secret_name: str) -> str:
+    """Retrieve a secret value from Google Secret Manager."""
+    project_id = os.getenv("PROJECT_ID", None)  # Replace with your GCP Project ID
+    client = secretmanager.SecretManagerServiceClient()
+    secret_path = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
+    response = client.access_secret_version(name=secret_path)
+    return response.payload.data.decode("UTF-8")
+
 
 def main():
-    # Service account setup (using environment variable)
-    service_account_json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON")
-    print("Service Account:")
-    print(service_account_json)
+    # Retrieve the Gemini API Key from Secret Manager
+    GEMINI_API_KEY = get_secret("gemini-sa-key")
+    print("GEMINI_API_KEY:")
+    print(GEMINI_API_KEY)
 
     dataframe = pd.read_csv("data/dataTest.csv")
     dataframe = dataframe.drop(columns=["Unnamed: 0"])
